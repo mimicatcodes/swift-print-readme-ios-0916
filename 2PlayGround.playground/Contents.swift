1539b1eeb1393c555165c1f1ab97bac0e162beab
Mirim An <mimicat@Mirims-MacBook-Air.local>
2016-08-29 15:57:02 -0400

    Done

---
+++ 2PlayGround.playground/Contents.swift
@@ -0,0 +1,19 @@
+var favoriteWord = "best"
+
+var favoriteColor = "black"
+
+var favoriteMusician = "Maxwell"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+print("I'm printing this stuff")
+
+let color = "blue"
+print("The sky is \(color).")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+var skySentence = "The sky is \(color)."
+print(skySentence)
+ 
